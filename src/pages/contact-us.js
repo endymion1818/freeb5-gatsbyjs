23403c475fcad387f4b8a266d934bf09b24192f4
Ben Read <ben.read@indigotree.co.uk>
2017-09-12 21:41:18 +0100

    building out static pages as react components

---
+++ src/pages/contact-us.js
@@ -0,0 +1,11 @@
+import React from "react"
+import g from "glamorous"
+import Link from "gatsby-link"
+
+import { rhythm } from "../utils/typography"
+
+export default ({ data }) => {
+  return (
+    Have any news, artwork submissions or want to help the campaign? Use this form to eMail us or simply email <freebabylon5@aol.com>
+  )
+}
--- src/pages/news-room.js
+++ src/pages/news-room.js
@@ -7,35 +7,19 @@ import { rhythm } from "../utils/typography"
 export default ({ data }) => {
   return (
     <div>
-    <div className="col-md-12 mission-text">
-      <h1 className="center">
-        Babylon 5 is a game-changing, ground breaking, emmy-award winning TV series that is in danger of becoming lost and forgotten.
-      </h1>
-
-      <p className="text-center">
-        We are not going to let that happen.
-      </p>
-
-      <p className="text-center">
-        Please <a className="btn btn-join" title="Join Us!" href="http://freeb5:8888/join/"><strong>Join us</strong></a> and help #FreeBabylon5
-      </p>
-
-      <p>
-        &nbsp;
-      </p>
-    </div>
       <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
-        Recent Posts
+        All Articles:
       </g.H1>
       <h4>
         {data.allMarkdownRemark.totalCount} Posts
       </h4>
       {data.allMarkdownRemark.edges.map(({ node }) =>
-        <div>
+        <article>
         <Link
           to={node.fields.slug}
           css={{ textDecoration: `none`, color: `inherit` }}
         >
+          <span>{node.frontmatter.type}</span>
           <g.H3 marginBottom={rhythm(1 / 4)}>
             {node.frontmatter.title}{" "}
             <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
@@ -44,24 +28,22 @@ export default ({ data }) => {
             {node.excerpt}
           </p>
           </Link>
-        </div>
+        </article>
       )}
     </div>
   )
 }
 
 export const query = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-        sort: {fields: [frontmatter___date], order: DESC}
-        limit: 5
-      ) {
+  query blogQuery {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
       totalCount
       edges {
         node {
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+            type
           }
           fields {
             slug
