bdc24c0c0399bc6d17afe05f49905ba220aa7a98
Ben Read <ben.read@indigotree.co.uk>
2017-10-16 21:14:37 +0100

    link in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "gatsby-link"
 import styled from "styled-components"
 import '../global-styles.js'
 
@@ -10,7 +11,10 @@ const Container = styled.section`
   margin: 0 auto;
   color: white;
   min-height: 80vh;
-
+  
+  a {
+    color: #428bca;
+  }
   @media (min-width: 768px) {
     display: flex;
   }
@@ -31,5 +35,6 @@ export default ({ data }) =>
       <img src={image404} alt="Susan Ivanova in Ops" width="100%"/>
       <h1>Absolutely nothing happened in Sector 404 today.</h1>
       <h2>I repeat, nothing happened in Sector 404.</h2>
+      <p>I'm sorry, I changed a lot of the post addresses when I moved this site over to some new hosting this month. Please check out our <Link to="/news-room">news room</Link> if you're looking for something specific.</p>
     </Main>
   </Container>
