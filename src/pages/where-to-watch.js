0e60f0253df1406cbd301e70cdc35c1f2f14c674
Benjamin Read <endymion1818@gmail.com>
2018-05-20 17:05:35 +0100

    watch page rejig, new post about twitter acct

--- src/pages/where-to-watch.js
+++ src/pages/where-to-watch.js
@@ -35,21 +35,16 @@ const TextLink = styled(Link)`
 export default ({ data }) =>
     <Container>
     <Main>
-      <h3>Babylon 5 isn&#8217;t on the air that often these days, so you might want to check out Amazon Video, iTunes or other suppliers in your country. We&#8217;re campaigning for Netflix to add Babylon 5 to their catalogue, so [please request it now!]</h3>
+      <h2>Amazon Prime Video</h2>
+      <p>We heard via comingsoon.com and other sources that Warner Bros have struck a deal to show Babylon 5 on Amazon Prime Video begining in July 2018. We're not sure how long ths deal might be available for, so if you're an Amazon Prime customer, get it whilst you can.</p>
 
-      <h2>United States</h2>
+      <a href="https://www.amazon.com/Prime-Instant-Video/">Watch now!</a>
 
-      <p>In April 2017 we heard that <TextLink href="https://www.go90.com/shows/babylon5">Go90 streaming service from Verizon</TextLink> will stream Babylon 5 as part of a deal with Warner Bros.</p>
-
-      <p>US TV channel Comet TV has been showing The Lost Tales as well. Please tell us if this is still happening.</p>
-
-      <h2>International</h2>
-
-      <p>TV stations in the UK, New Zealand and Germany have recently aired all 5 seasons. Please let us know if you hear of anywhere else that&#8217;s showing the series!</p>
+      <p>This could not have come at a better time, as we heard that Go90 in the US and Pick in the UK have recently stopped showing the series. </p>
 
       <h2>Let us know!</h2>
 
-      We&#8217;ll try to keep you updated when we hear of airings. In the meantime, <TextLink href="https://facebook.com/freebabylon5">keep in touch via our Facebook page</TextLink>, as it&#8217;s likely that announcements will be made there first!
+      <p>We&#8217;ll try to keep you updated when we hear of airings. In the meantime, <TextLink href="https://facebook.com/freebabylon5">keep in touch via our Facebook page</TextLink>, as it&#8217;s likely that announcements will be made there first!</p>
     </Main>
     <Aside>
     </Aside>
