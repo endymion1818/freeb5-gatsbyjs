f07f12eeb357b82a246f58484f631450ca786181
Benjamin Read <endymion1818@gmail.com>
2019-06-01 08:51:04 +0100

    improved font stack;

--- src/components/Templates/Layout.tsx
+++ src/components/Templates/Layout.tsx
@@ -1,8 +1,9 @@
 import { Helmet } from "react-helmet"
-import * as React from "react"
+import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import { createGlobalStyle } from "styled-components"
 import serpentine from '../../assets/serpentine.woff2'
+
 interface StaticQueryProps {
   site: {
     siteMetadata: {
@@ -17,17 +18,22 @@ const GlobalStyle = createGlobalStyle`
      font-family: 'SerpentineMedium';
      src: url(${serpentine});
   }
+  @font-face {
+    font-family: system;
+    font-style: normal;
+    font-weight: 300;
+    src: local(".SFNSText-Light"), local(".HelveticaNeueDeskInterface-Light"), local(".LucidaGrandeUI"), local("Ubuntu Light"), local("Segoe UI Light"), local("Roboto-Light"), local("DroidSans"), local("Tahoma");
+  }
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-family: system;
   }
   h1,
   h2,
   h3,
   h4,
   h5,
-  h6 
-  {
+  h6 {
     font-family: SerpentineMedium;
   }
 `
