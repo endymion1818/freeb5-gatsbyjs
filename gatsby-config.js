d39c859b1d642e629ae0905fcb70ac3183f9c45d
Benjamin Read <endymion1818@gmail.com>
2019-06-05 21:35:18 +0100

    adding standard plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
+const siteTitle = `#FreeBabylon5.com`
+
 module.exports = {
   siteMetadata: {
     pathPrefix: '/',
-    title: `#FreeBabylon5.com`,
+    title: siteTitle,
   },
   plugins: [
     `gatsby-plugin-typescript`,
@@ -16,5 +18,39 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-twitter`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
+    `gatsby-transformer-json`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-eslint`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-webpack-size`,
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /images\/.*\.svg$/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: siteTitle,
+        short_name: siteTitle,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
   ],
 }
