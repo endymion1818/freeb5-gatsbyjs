b36645b92aa1ffb1bbcb8abe27b36294511dfc83
Benjamin Read <endymion1818@gmail.com>
2019-06-08 08:17:25 +0100

    basepath changed to pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -78,11 +78,11 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
       name: `slug`,
       node,
-      value,
+      value: slug,
     })
   }
 }
